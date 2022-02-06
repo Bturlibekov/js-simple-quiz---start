864c1d8e5af0eb82dee5b285e64ede59a3c78ab3
Bturlibekov <80024511+Bturlibekov@users.noreply.github.com>
2022-02-06 18:11:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,130 @@
+const questions = [
+	{
+		question: "Какой язык работает в браузере?",
+		answers: ["Java", "C", "Python", "JavaScript"],
+		correct: 4,
+	},
+	{
+		question: "Что означает CSS?",
+		answers: [
+			"Central Style Sheets",
+			"Cascading Style Sheets",
+			"Cascading Simple Sheets",
+			"Cars SUVs Sailboats",
+		],
+		correct: 2,
+	},
+	{
+		question: "Что означает HTML?",
+		answers: [
+			"Hypertext Markup Language",
+			"Hypertext Markdown Language",
+			"Hyperloop Machine Language",
+			"Helicopters Terminals Motorboats Lamborginis",
+		],
+		correct: 1,
+	},
+	{
+		question: "В каком году был создан JavaScript?",
+		answers: ["1996", "1995", "1994", "все ответы неверные"],
+		correct: 2,
+	},
+];
+
+const headerContainer = document.querySelector('#header');
+const listContainer = document.querySelector('#list');
+const submitBtn = document.querySelector('#submit');
+
+let score = 0;
+let questionIndex = 0;
+
+clearPage()
+showQuestion()
+submitBtn.onclick = checkAnswer;
+
+
+function clearPage() {
+	headerContainer.innerHTML = '';
+	listContainer.innerHTML = '';
+}
+
+function showQuestion() {
+	const headerTemplate = `<h2 class="title">%title%</h2>`
+	const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+	headerContainer.innerHTML = title;
+
+
+	let answerNumber = 1;
+	for (answerText of questions[questionIndex]['answers']) {
+		const questionTemplate =
+			`<li>
+				<label>
+					<input value="%number%" type="radio" class="answer" name="answer" />
+						<span>%answer%</span>
+				</label>
+			</li>`;
+
+		const answerHtml = questionTemplate.replace('%answer%', answerText).replace('%number%', answerNumber);
+
+		listContainer.innerHTML += answerHtml;
+		answerNumber++;
+	}
+
+
+}
+
+function checkAnswer() {
+	const checkedRadio = listContainer.querySelector('input[type="radio"]:checked');
+
+	if (!checkedRadio) {
+		submitBtn.blur();
+		return;
+	}
+
+	const userAnswer = parseInt(checkedRadio.value);
+	if (userAnswer === questions[questionIndex]['correct']) {
+		score++;
+	}
+
+	if (questionIndex !== questions.length - 1) {
+		questionIndex++;
+		clearPage();
+		showQuestion()
+		return;
+	} else {
+		clearPage();
+		showResults();
+	}
+}
+
+function showResults() {
+	const resultTemplate =
+		`<h2 class="title">%title%</h2>
+	     <h3 class="summary">%message%</h3>
+	     <p class="result">%result%</p>`;
+
+	let title, message;
+	if (score === questions.length) {
+		title = 'Поздравляем';
+		message = 'Вы ответили на все вопросы';
+	} else if ((score * 100) / questions.length >= 50) {
+		title = 'Неплохой результат!';
+		message = 'Вы дали более половины правильных ответов';
+	} else {
+		title = 'Стоит посторатся';
+		message = 'Пока у вас меньшее половины правильных ответов';
+	}
+
+	let result = `${score} из ${questions.length}`;
+
+	const finalMessage = resultTemplate
+		.replace('%title%', title)
+		.replace('%message%', message)
+		.replace('%result%', result);
+
+	headerContainer.innerHTML = finalMessage;
+
+	submitBtn.blur();
+	submitBtn.innerHTML = 'Начать заново';
+	submitBtn.onclick = () => history.go();
+}
